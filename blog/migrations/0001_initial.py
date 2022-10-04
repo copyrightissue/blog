22b376bf35cd6c51be861588c7b21b5168c528ca
username34 <chris.rogers@mail.helenacollege.edu>
2022-10-04 11:46:26 -0600

    added CRUD model

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -21,6 +21,7 @@ class Migration(migrations.Migration):
                 ('title', models.CharField(max_length=200)),
                 ('body', models.TextField()),
                 ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+
             ],
         ),
     ]
